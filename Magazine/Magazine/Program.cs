68784a971b77f513236e1abf0710d30019996770
Altan013 <altanai@code.edu.az>
2022-01-11 11:10:21 +0400

    first commit

---
+++ Magazine/Magazine/Program.cs
@@ -0,0 +1,49 @@
+﻿using Market.Models;
+using System;
+
+namespace Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister operation = new CashRegister(500, Enums.Currency.AZN, Enums.PaymentType.Cash);
+
+            operation.AddNewSale(100, Enums.Currency.AZN);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(100, Enums.Currency.TRY);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(100, Enums.Currency.USD);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(100, Enums.Currency.EUR);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.AZN);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.TRY);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.USD);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.RemoveSale(100, Enums.Currency.EUR);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+
+            operation.AddNewSale(10, Enums.Currency.USD);
+            Console.WriteLine(operation);
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
